56052ce7453211952060db714c4a286b236b6be0
duong-jason <reddkingdom@csu.fullerton.edu>
2021-10-12 19:09:43 -0700

    honestly im lost in this project

--- manager.js
+++ manager.js
@@ -23,12 +23,17 @@ class Manager {
     }
 
     decode() {
-        switch(this.name) {
-            case "Insertion" : test1(this); break;
-            case "Selection" : test2(this); break;
-            case "GoldsPore" : test3(this); break;
-            default : break;
-        }
+		this.restore_state();
+		if (!done) {
+			let t = 0;
+			switch(this.name) {
+				case "Insertion" : test1(this); break;
+				case "Selection" : test2(this); t+=200; break;
+				case "GoldsPore" : test3(this); t+=400; break;
+				default : break;
+			}
+			this.save_state({ idx: ++i, buffer: A }, t);
+		}
     }
 }
 
@@ -36,50 +41,35 @@ class Manager {
 var A, i, n, done, dbit;
 
 function test1(alg) {
-	alg.restore_state();
-
-    if (!done) {
-        let deck = A[i], hand = i-1;
-        // swaps elements in hand as long as deck elm < current hand elm 
-        while (hand >= 0 && A[hand] > deck) {
-            A[hand+1] = A[hand--];
-        }
-        A[hand+1] = deck;
-		alg.save_state({ idx: ++i, buffer: A }, 0);
-    }
+	let deck = A[i], hand = i-1;
+	// swaps elements in hand as long as deck elm < current hand elm 
+	while (hand >= 0 && A[hand] > deck) {
+		A[hand+1] = A[hand--];
+	}
+	A[hand+1] = deck;
 }
 
 function test2(alg) {
-	alg.restore_state();
-
-    if (!done) {
-        let min = i;
-        // searches for the minimum element along A[0...n-i-1] elements
-        for (let j = i+1; j < A.length; ++j) {
-            if (A[j] < A[min]) min = j;
-        }
-        [A[i], A[min]] = [A[min], A[i]];
-		alg.save_state({ idx: ++i, buffer: A }, 200);
-    }
+	let min = i;
+	// searches for the minimum element along A[0...n-i-1] elements
+	for (let j = i+1; j < n; ++j) {
+		if (A[j] < A[min]) min = j;
+	}
+	[A[i], A[min]] = [A[min], A[i]];
 }
 
 function test3(alg) {
-	alg.restore_state();
-
-    if (!done) {
-        dbit = true;
-        for (let k = 0; k < A.length; k+=2) { // even half-passes
-            if (A[k+1] < A[k]) {
-                dbit = false;
-                [A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
-            }
-        }
-        for (let k = 1; k < A.length; k+=2) { // odd half-passes
-            if (A[k+1] < A[k]) {
-                dbit = false;
-                [A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
-            }
-        }
-		alg.save_state({ idx: ++i, buffer: A, done: dbit }, 400);
-    }
+	dbit = true;
+	for (let k = 0; k < n; k+=2) { // even half-passes
+		if (A[k+1] < A[k]) {
+			dbit = false;
+			[A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
+		}
+	}
+	for (let k = 1; k < n; k+=2) { // odd half-passes
+		if (A[k+1] < A[k]) {
+			dbit = false;
+			[A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
+		}
+	}
 }
