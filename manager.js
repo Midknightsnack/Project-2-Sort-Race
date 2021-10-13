f48a75e9592c0c50c40215874577ac02bea29703
duong-jason <reddkingdom@csu.fullerton.edu>
2021-10-12 19:14:05 -0700

    honestly im lost in this project

--- manager.js
+++ manager.js
@@ -15,61 +15,61 @@ class Manager {
     }
 
     restore_state() {
-        A = this.CB.buffer;
-        i = this.CB.idx;
-        n = this.CB.buffer.length;
-        dbit = this.CB.done;
-        done = solved(A);
+		A = this.CB.buffer;
+		i = this.CB.idx;
+		n = this.CB.buffer.length;
+		dbit = this.CB.done;
+		done = solved(A);
     }
 
     decode() {
-        this.restore_state();
-        if (!done) {
-            let t = 0;
-            switch(this.name) {
-                case "Insertion" : test1(); break;
-                case "Selection" : test2(); t+=200; break;
-                case "GoldsPore" : test3(); t+=400; break;
-                default : break;
-            }
-            this.save_state({ idx: ++i, buffer: A }, t);
-        }
+		this.restore_state();
+		if (!done) {
+			let t = 0;
+			switch(this.name) {
+				case "Insertion" : test1(); break;
+				case "Selection" : test2(); t+=200; break;
+				case "GoldsPore" : test3(); t+=400; break;
+				default : break;
+			}
+			this.save_state({ idx: ++i, buffer: A }, t);
+		}
     }
 }
 
 // global memory
 var A, i, n, done, dbit;
 
-function test1(alg) {
-    let deck = A[i], hand = i-1;
-    // swaps elements in hand as long as deck elm < current hand elm 
-    while (hand >= 0 && A[hand] > deck) {
-        A[hand+1] = A[hand--];
-    }
-    A[hand+1] = deck;
+function test1() {
+	let deck = A[i], hand = i-1;
+	// swaps elements in hand as long as deck elm < current hand elm 
+	while (hand >= 0 && A[hand] > deck) {
+		A[hand+1] = A[hand--];
+	}
+	A[hand+1] = deck;
 }
 
-function test2(alg) {
-    let min = i;
-    // searches for the minimum element along A[0...n-i-1] elements
-    for (let j = i+1; j < n; ++j) {
-        if (A[j] < A[min]) min = j;
-    }
-    [A[i], A[min]] = [A[min], A[i]];
+function test2() {
+	let min = i;
+	// searches for the minimum element along A[0...n-i-1] elements
+	for (let j = i+1; j < n; ++j) {
+		if (A[j] < A[min]) min = j;
+	}
+	[A[i], A[min]] = [A[min], A[i]];
 }
 
-function test3(alg) {
-    dbit = true;
-    for (let k = 0; k < n; k+=2) { // even half-passes
-        if (A[k+1] < A[k]) {
-            dbit = false;
-            [A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
-        }
-    }
-    for (let k = 1; k < n; k+=2) { // odd half-passes
-        if (A[k+1] < A[k]) {
-            dbit = false;
-            [A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
-        }
-    }
+function test3() {
+	dbit = true;
+	for (let k = 0; k < n; k+=2) { // even half-passes
+		if (A[k+1] < A[k]) {
+			dbit = false;
+			[A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
+		}
+	}
+	for (let k = 1; k < n; k+=2) { // odd half-passes
+		if (A[k+1] < A[k]) {
+			dbit = false;
+			[A[k+1], A[k]] = [A[k], A[k+1]]; // swaps the current and next element
+		}
+	}
 }
